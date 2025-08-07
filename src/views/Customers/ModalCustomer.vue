db97bc843b06e3a21cd7506c531d84d1eedee32a
Your Name <styvesdaudet@gmail.com>
2025-08-07 15:07:56 +0100

    details

--- src/views/Customers/ModalCustomer.vue
+++ src/views/Customers/ModalCustomer.vue
@@ -42,16 +42,8 @@
                 {{ $t('personalInformation') }}
               </h5>
               <div class="grid grid-cols-1 gap-x-6 gap-y-5 lg:grid-cols-3">
-                <div>
-                  <Select
-                    :is-required="true"
-                    :lb="$t('gender')"
-                    v-model="customerForm.gender"
-                    :options="genders"
-                  />
-                </div>
 
-                <div>
+                 <div>
                   <Input
                     :lb="$t('FirstName')"
                     :id="'name'"
@@ -70,6 +62,16 @@
                     :is-required="true"
                   />
                 </div>
+                <div>
+                  <Select
+                    :is-required="true"
+                    :lb="$t('gender')"
+                    v-model="customerForm.gender"
+                    :options="genders"
+                  />
+                </div>
+
+               
 
                 <div>
                   <InputDatePicker
@@ -81,12 +83,11 @@
                 </div>
 
                 <div>
-                   <Input
+                  <Select
                     :is-required="true"
                     :lb="$t('country')"
-                    :id="'country'"
-                    :forLabel="'country'"
                     v-model="customerForm.country"
+                    :options="africanCountries"
                   />
                 </div>
 
@@ -311,6 +312,63 @@ const genders = ref([
   { label: t('Female'), value: 'female' },
 ])
 
+const africanCountries = computed(() => [
+    { value: 'DZ', label: t('countries_list.dz') },
+    { value: 'AO', label: t('countries_list.ao') },
+    { value: 'BJ', label: t('countries_list.bj') },
+    { value: 'BW', label: t('countries_list.bw') },
+    { value: 'BF', label: t('countries_list.bf') },
+    { value: 'BI', label: t('countries_list.bi') },
+    { value: 'CV', label: t('countries_list.cv') },
+    { value: 'CM', label: t('countries_list.cm') },
+    { value: 'CF', label: t('countries_list.cf') },
+    { value: 'TD', label: t('countries_list.td') },
+    { value: 'KM', label: t('countries_list.km') },
+    { value: 'CD', label: t('countries_list.cd') },
+    { value: 'CG', label: t('countries_list.cg') },
+    { value: 'CI', label: t('countries_list.ci') },
+    { value: 'DJ', label: t('countries_list.dj') },
+    { value: 'EG', label: t('countries_list.eg') },
+    { value: 'GQ', label: t('countries_list.gq') },
+    { value: 'ER', label: t('countries_list.er') },
+    { value: 'SZ', label: t('countries_list.sz') },
+    { value: 'ET', label: t('countries_list.et') },
+    { value: 'GA', label: t('countries_list.ga') },
+    { value: 'GM', label: t('countries_list.gm') },
+    { value: 'GH', label: t('countries_list.gh') },
+    { value: 'GN', label: t('countries_list.gn') },
+    { value: 'GW', label: t('countries_list.gw') },
+    { value: 'KE', label: t('countries_list.ke') },
+    { value: 'LS', label: t('countries_list.ls') },
+    { value: 'LR', label: t('countries_list.lr') },
+    { value: 'LY', label: t('countries_list.ly') },
+    { value: 'MG', label: t('countries_list.mg') },
+    { value: 'MW', label: t('countries_list.mw') },
+    { value: 'ML', label: t('countries_list.ml') },
+    { value: 'MR', label: t('countries_list.mr') },
+    { value: 'MU', label: t('countries_list.mu') },
+    { value: 'MA', label: t('countries_list.ma') },
+    { value: 'MZ', label: t('countries_list.mz') },
+    { value: 'NA', label: t('countries_list.na') },
+    { value: 'NE', label: t('countries_list.ne') },
+    { value: 'NG', label: t('countries_list.ng') },
+    { value: 'RW', label: t('countries_list.rw') },
+    { value: 'ST', label: t('countries_list.st') },
+    { value: 'SN', label: t('countries_list.sn') },
+    { value: 'SC', label: t('countries_list.sc') },
+    { value: 'SL', label: t('countries_list.sl') },
+    { value: 'SO', label: t('countries_list.so') },
+    { value: 'ZA', label: t('countries_list.za') },
+    { value: 'SS', label: t('countries_list.ss') },
+    { value: 'SD', label: t('countries_list.sd') },
+    { value: 'TZ', label: t('countries_list.tz') },
+    { value: 'TG', label: t('countries_list.tg') },
+    { value: 'TN', label: t('countries_list.tn') },
+    { value: 'UG', label: t('countries_list.ug') },
+    { value: 'ZM', label: t('countries_list.zm') },
+    { value: 'ZW', label: t('countries_list.zw') }
+]);
+
 const loading = ref(false)
 
 
@@ -319,7 +377,7 @@ const getEmptyCustomerForm = (): CustomerForm => ({
   first_name: '',
   last_name: '',
   date_of_birth: '',
-  country: '',
+  country: 'CM',
   national_id_number: '',
   email: '',
   phone_number: '',
