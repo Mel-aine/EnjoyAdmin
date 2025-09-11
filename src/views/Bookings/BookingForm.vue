655438749f4802fc615c0006b7629adbf2208e2c
Your Name <styvesdaudet@gmail.com>
2025-09-11 16:19:15 +0100

    boooking

--- src/views/Bookings/BookingForm.vue
+++ src/views/Bookings/BookingForm.vue
@@ -34,7 +34,7 @@
                 </div>
 
                 <!-- Nights -->
-                <div class="flex flex-col ">
+                <div class="flex flex-col w-full">
                   <Input :lb="$t('nights')" :disabled="true" :modelValue="numberOfNights.toString()" />
                 </div>
 
