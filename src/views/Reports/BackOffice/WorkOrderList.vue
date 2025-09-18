9e63dc088c393bff2f6989de4fcfa6e32af7a2ee
KAMGA <tetakamgasteve@gmail.com>
2025-09-18 16:55:12 +0100

    update

--- src/views/Reports/BackOffice/WorkOrderList.vue
+++ src/views/Reports/BackOffice/WorkOrderList.vue
@@ -5,6 +5,9 @@
         <h1 class="text-2xl font-bold text-gray-900 dark:text-white mb-2">
           {{ $t('Work Order List') }}
         </h1>
+        <!-- <p class="text-gray-600 dark:text-gray-400">
+          {{ $t('reports.backOffice.workOfficeReportDescription') }}
+        </p> -->
       </div>
 
       <!-- Filters -->
