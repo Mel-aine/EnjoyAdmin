0e7ae68d3a1beca2a5278051cde96d63a9cf0449
aroncloud <arouncloud@gmail.com>
2025-10-08 09:27:34 +0100

    Update App.vue
    Signed-off-by: aroncloud <arouncloud@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@
   </ThemeProvider>
 </template>
 
+
 <script setup lang="ts">
 import ThemeProvider from './components/layout/ThemeProvider.vue'
 import SidebarProvider from './components/layout/SidebarProvider.vue'
