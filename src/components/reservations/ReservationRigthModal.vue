716c78617a660a7d81d21d742e5f635851af841d
Your Name <styvesdaudet@gmail.com>
2025-11-05 10:12:25 +0100

    reservation

--- src/components/reservations/ReservationRigthModal.vue
+++ src/components/reservations/ReservationRigthModal.vue
@@ -1,5 +1,5 @@
 <template>
-    <div v-if="isOpen" class="fixed inset-0 z-[99999] h-full overflow-hidden">
+    <div v-if="isOpen" class="fixed inset-0 z-[95] h-full overflow-hidden">
         <!-- Backdrop -->
         <div class="absolute inset-0 bg-black/25 bg-opacity-50 transition-opacity h-full"></div>
 
