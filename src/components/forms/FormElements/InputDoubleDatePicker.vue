c07336f5dd3b8dcb75143367d82047c96e3530a7
Your Name <styvesdaudet@gmail.com>
2025-11-20 09:16:25 +0100

    stats

--- src/components/forms/FormElements/InputDoubleDatePicker.vue
+++ src/components/forms/FormElements/InputDoubleDatePicker.vue
@@ -11,7 +11,7 @@
         @on-change="updateValue"
         @on-ready="onReady"
         :class="[
-          'dark:bg-dark-900 h-11 w-full rounded-lg appearance-none border border-black/50 bg-transparent px-4 py-2.5 pr-10 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-purple-300 focus:outline-hidden focus:ring-3 focus:ring-purple-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-purple-800',
+          'h-11 w-full rounded-lg appearance-none border border-gray-300 bg-white px-4 py-2.5 pr-10 text-sm text-gray-900 shadow-theme-xs placeholder:text-gray-500 focus:border-purple-300 focus:outline-hidden focus:ring-3 focus:ring-purple-500/10 dark:border-gray-700 dark:bg-dark-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-purple-800',
           props.customClass
         ]"
         :placeholder="$t('selected_period')"
