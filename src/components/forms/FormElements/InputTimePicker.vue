a9df95431cdbf96894a6b49b98dd9fb37a2b2468
Mel-aine <melaineevans7@gmail.com>
2025-11-28 14:28:38 +0100

    commit

--- src/components/forms/FormElements/InputTimePicker.vue
+++ src/components/forms/FormElements/InputTimePicker.vue
@@ -16,7 +16,7 @@
         @on-change="handleChange"
         @on-close="handleClose"
         :class="[
-          'h-11 w-full appearance-none border border-gray-300 bg-white px-4 py-2.5 pr-11 text-sm text-gray-900 shadow-theme-xs placeholder:text-gray-500 focus:border-purple-300 focus:outline-hidden focus:ring-3 focus:ring-purple-500/10 dark:border-gray-700 dark:bg-dark-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-purple-800',
+          'h-11 w-full appearance-none border border-black/50 bg-white px-4 py-2.5 pr-11 text-sm text-gray-900 shadow-theme-xs placeholder:text-gray-500 focus:border-purple-300 focus:outline-hidden focus:ring-3 focus:ring-purple-500/10 dark:border-gray-700 dark:bg-dark-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-purple-800',
         props.customClass
         ]"
 
