ebafedd6175dd16ac8bdb75017e6ceec5d3c011d
Your Name <styvesdaudet@gmail.com>
2026-01-20 08:33:53 +0100

    amend stay

--- src/components/reservations/foglio/AmendStay.vue
+++ src/components/reservations/foglio/AmendStay.vue
@@ -77,10 +77,8 @@
 
                     <!-- Date and Nights Fields -->
                     <div class="mb-7 grid grid-cols-1 md:grid-cols-2 gap-4">
-                        <!-- <InputDatePicker :title="$t('arrivalDate')" v-model="formData.newArrivalDate"
-                            :is-required="true" :disabled="isCheckedIn" /> -->
                         <InputDatePicker :title="$t('arrivalDate')" v-model="formData.newArrivalDate"
-                            :is-required="true" />
+                            :is-required="true" :disabled="isCheckedIn" />
                         <InputDatePicker :title="$t('departureDate')" v-model="formData.newDepartureDate"
                             :is-required="true" />
                         <Input input-type="number" :lb="$t('nights')" :title="$t('nights')" v-model="formData.nights"
