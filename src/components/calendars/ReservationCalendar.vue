d6e88933f078d2b02199116e5d32a465938253e5
Mel-aine <melaineevans7@gmail.com>
2026-04-21 15:35:53 +0100

    commit

--- src/components/calendars/ReservationCalendar.vue
+++ src/components/calendars/ReservationCalendar.vue
@@ -700,7 +700,7 @@
             }}</span>
           </div>
          
-          <div v-if="hasCredirLedgerTransfer(tooltipReservation)" class="flex justify-between text-purple-400">
+          <!-- <div v-if="hasCredirLedgerTransfer(tooltipReservation)" class="flex justify-between text-purple-400">
             <span class="font-medium flex items-center gap-1">
               <svg class="w-3 h-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3"/>
@@ -710,7 +710,7 @@
             <span class="font-bold">
               {{ formatCurrency(tooltipReservation?.balance_summary?.creditLedgerTransferred || 0) }}
             </span>
-          </div>
+          </div> -->
         </div>
       </div>
       <!-- Flèche -->
